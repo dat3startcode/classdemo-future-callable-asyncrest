b428b409fa12e138db0eb7ffeaba5152703f6937
Lars Mortensen (LAM - Adjunkt - Cphbusiness) <lam@cphbusiness.dk>
2019-11-06 10:09:03 +0100

    cleaned up

--- src/main/java/rest/PingResource.java
+++ src/main/java/rest/PingResource.java
@@ -7,13 +7,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
@@ -21,35 +17,47 @@ import javax.ws.rs.core.MediaType;
 @Path("serverstatus")
 public class PingResource {
 
-  private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  private static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
   
-  @Context
-  private UriInfo context;
+
+  // Green (Yellow) students use this simple (no async strategy)
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("simple")
+  public String getStatus(){
+      return "{\"msg\":\"Make me return server status from all servers\"}";
+  }
   
+  // Red (Yellow) students use this async strategy, as recomended by the jersey documentation
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public void getServerStatus(@Suspended final AsyncResponse ar) {
-    ExecutorService es = Executors.newSingleThreadExecutor();
-      es.submit(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          Thread.sleep(2000);  //Simulate a long running process
-        } catch (InterruptedException ex) {
-          Logger.getLogger(PingResource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        ar.resume("{\"msg\":\"Make me return server status from all servers\"}");
-        es.shutdown();
-      }
-    });
+     new Thread(() -> {
+            try {
+                Thread.sleep(2000);  //Simulate a long running process
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PingResource.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            ar.resume("{\"msg\":\"Make me return server status from all servers\"}");
+        }).start();
   }
   
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("solution")
   public void getServerStatus2(@Suspended final AsyncResponse ar) throws Exception {
-    List<String> res = parallel.ParallelPinger.getStatusFromAllServers();  
-    ar.resume("{\"status\":\""+gson.toJson(res)+"\"}");
+    System.out.println("---- A -------");
+        new Thread(() -> {
+            List<String> res = null;
+            try {
+                res = parallel.ParallelPinger.getStatusFromAllServers();
+            } catch (Exception ex) {
+                Logger.getLogger(PingResource.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("---- B -------" + res);
+            ar.resume("{\"status\":\"" + GSON.toJson(res) + "\"}");
+        }).start();
+        System.out.println("---- C -------");
   }
     
 }
